4f6169089a4bfe94854b50d6c8f4b4d18f725b12
rockcutter <rockcutter414@gmail.com>
2023-06-13 23:03:46 +0900

    [change] clientディレクトリとbackendディレクトリを作って作ってもらっていたファイル群をclient/に移動

---
+++ client/Mosa_CppKanojo/Main.cpp
@@ -0,0 +1,53 @@
+﻿# include <Siv3D.hpp>
+
+using App = SceneManager<String>;
+
+// タイトルシーン
+class Title : public App::Scene
+{
+public:
+
+	// コンストラクタ（必ず実装）
+	Title(const InitData& init)
+		: IScene{ init }
+	{
+
+	}
+
+	// 更新関数（オプション）
+	void update() override
+	{
+
+	}
+
+	// 描画関数（オプション）
+	void draw() const override
+	{
+		Scene::SetBackground(ColorF{ 0.3, 0.4, 0.5 });
+
+		FontAsset(U"TitleFont")(U"My Game").drawAt(400, 100);
+
+		Circle{ Cursor::Pos(), 50 }.draw(Palette::Orange);
+	}
+};
+
+void Main()
+{
+	FontAsset::Register(U"TitleFont", 60, Typeface::Heavy);
+
+	// シーンマネージャーを作成
+	App manager;
+
+	// タイトルシーン（名前は "Title"）を登録
+	manager.add<Title>(U"Title");
+
+	while (System::Update())
+	{
+		// 現在のシーンを実行
+		// シーンに実装した .update() と .draw() が実行される
+		if (not manager.update())
+		{
+			break;
+		}
+	}
+}
