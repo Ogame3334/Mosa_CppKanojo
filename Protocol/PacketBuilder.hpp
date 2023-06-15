dfddcedf978ad1dfa7517c683a00383499dba730
rockcutter <rockcutter414@gmail.com>
2023-06-15 21:06:52 +0900

    [add] PacketBuilderを作成

---
+++ Protocol/PacketBuilder.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "OperationType.hpp"
+#include <string_view>
+#include <string>
+#include <stdexcept>
+#include <sstream>
+#include <iomanip>
+
+namespace FruitsGroove{
+class PacketBuilder{
+private:
+    OperationType opType;
+    std::string etc;
+    
+public:
+    PacketBuilder(){};
+
+    PacketBuilder& SetOperation(OperationType op){
+        this->opType = op;
+        return *this;
+    }
+
+    PacketBuilder& SetEtcString(std::string_view str){
+        this->etc = str;
+        return *this;
+    }
+
+    std::string Build(){
+        std::stringstream packetStringStream{""};
+        
+        auto operationTypeNum = static_cast<int>(this->opType);
+        if(operationTypeNum > 99){
+            throw std::logic_error{"operationは99を超えないはずです"};
+        }
+
+        packetStringStream << std::setw(2) << std::setfill('0') << operationTypeNum;
+
+        packetStringStream << this->etc;
+    }
+};
+}
