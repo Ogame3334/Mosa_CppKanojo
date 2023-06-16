e2ecee09ea24cc52adba9a4f7f7b851ca1a3c3fe
rockcutter <rockcutter414@gmail.com>
2023-06-16 15:27:52 +0900

    [add]無効な文字列を検出時にNoneを入れるように修正

--- client/Mosa_CppKanojo/Protocol/PacketParser.hpp
+++ client/Mosa_CppKanojo/Protocol/PacketParser.hpp
@@ -6,7 +6,11 @@
 namespace FruitsGroove{
 Packet ParsePacket(std::string_view rawString){
     Packet packet;
-    packet.op = static_cast<OperationType>(stoi(std::string{rawString.substr(0, 2)}));
+	try{
+		packet.op = static_cast<OperationType>(stoi(std::string{rawString.substr(0, 2)}));
+	}catch(...){
+		packet.op = OperationType::None;
+	}
     packet.content = rawString.substr(2);
     return packet;
 }
