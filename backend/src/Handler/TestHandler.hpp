479b5022ce32c4567188d8b4199ab3db34e6f100
rockcutter <rockcutter414@gmail.com>
2023-06-17 17:05:04 +0900

    [add] Test用に処理を追加

--- backend/src/Handler/TestHandler.hpp
+++ backend/src/Handler/TestHandler.hpp
@@ -2,6 +2,10 @@
 #include "HandlerBase.hpp"
 #include <iostream>
 #include <ostream>
+#include <spdlog/spdlog.h>
+
+namespace asio = boost::asio;
+using boost::asio::ip::tcp;
 
 namespace FruitsGroove{
 class TestHandler: public HandlerBase{
@@ -15,8 +19,23 @@ class TestHandler: public HandlerBase{
         os(std::cout)
     {}
 
-    void Handle(std::string_view data) override{
-        this->os << data;
+    void Handle(std::unique_ptr<boost::asio::ip::tcp::socket> socket) override{
+        while(true){
+            boost::system::error_code error;
+            asio::streambuf receive_buffer;
+            spdlog::info("handle: socket read start");
+            asio::read(*socket, receive_buffer, asio::transfer_at_least(1), error);
+            if(!socket->available()) spdlog::warn("socket is not available", error.message());
+            if(!socket->is_open()) spdlog::warn("socket is not opened", error.message());
+
+            
+            if (error && error != asio::error::eof) {
+                spdlog::warn("receive failed: {}", error.message());
+                return;
+            }
+            const char* data = asio::buffer_cast<const char*>(receive_buffer.data());
+            spdlog::info(data);
+        }
     }
 };
 }
