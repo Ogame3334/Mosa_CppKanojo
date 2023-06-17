abf72ce4ce883cb00b78156bf46f19c12a43c1c5
rockcutter <rockcutter414@gmail.com>
2023-06-17 21:45:53 +0900

    [change] change Handle argument

--- backend/src/Handler/HandlerBase.hpp
+++ backend/src/Handler/HandlerBase.hpp
@@ -3,11 +3,13 @@
 #include <string_view>
 #include <boost/asio.hpp>
 #include <memory>
+class Room; //TODO #include "src/Room/Room.h" 実装時に置き換え
+
 
 namespace FruitsGroove{
     class HandlerBase{
     public:
-    virtual void Handle(std::string_view) = 0;
+    virtual void Handle(std::string_view, Room&) = 0;
     };
 }
 
