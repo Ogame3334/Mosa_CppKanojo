d09925094eee3d49728aaa401bb51b55ccf6cfd2
rockcutter <rockcutter414@gmail.com>
2023-06-17 17:06:22 +0900

    [change] udp -> tcpに変更

--- backend/src/Server/Server.cpp
+++ backend/src/Server/Server.cpp
@@ -1,8 +1,15 @@
 #include "Server.hpp"
+#include "src/Handler/HandlerProcessExecuter.hpp"
 #include <iostream>
 #include <sstream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <spdlog/spdlog.h>
+#include <thread>
+
+namespace asio = boost::asio;
+using boost::asio::ip::tcp;
+
 
 namespace FruitsGroove{
     Server::Server(HandlerPtr instance) : sock(this->io_service)
@@ -13,16 +20,40 @@ namespace FruitsGroove{
     void Server::Start(){
         using namespace boost::asio::ip;
 
-        this->sock = udp::socket{this->io_service, udp::endpoint(udp::v4(), 23292)};
+        tcp::acceptor acc(this->io_service, tcp::endpoint(tcp::v4(), 23292));
+        HandlerProcessExecuter handlerExecuter;
 
         for(;3;){ // <-可愛い！！！！
-            boost::array<char, 128> recv_buf;
-            udp::endpoint endpoint;
-            size_t len = sock.receive_from(boost::asio::buffer(recv_buf), endpoint);
+            spdlog::info("accepter start");
+            auto socket = std::make_unique<tcp::socket>(this->io_service);
 
-            std::stringstream ss;
-            ss.write(recv_buf.data(), len);
-            this->handerInstance->Handle(ss.str());
+            boost::system::error_code error;
+            acc.accept(*socket, error);
+
+            if(error){
+                spdlog::warn("tcp accepter: accept failed");
+                continue;
+            }
+            auto endpoint = socket->remote_endpoint();
+            spdlog::info("accepted from {}:{}", endpoint.address().to_string(), endpoint.port());
+
+            handlerExecuter.AddTask(std::make_unique<std::thread>(
+                [&](){
+                    this->handerInstance->Handle(std::move(socket));
+                }
+            ));
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            //// メッセージ受信
+            //asio::streambuf receive_buffer;
+            //asio::read(*socket, receive_buffer, asio::transfer_at_least(1), error);
+
+            //if (error && error != asio::error::eof) {
+                //spdlog::warn("receive failed {}", error.message());
+            //}
+            //const char* data = asio::buffer_cast<const char*>(receive_buffer.data());
+            //spdlog::info(data);
         }
     }
 
