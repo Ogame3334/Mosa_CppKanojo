de7d6c5834cb068ae7ca4cc1f073a8228f2aaed7
rockcutter <rockcutter414@gmail.com>
2023-06-17 16:57:13 +0900

    [add] log処理を追加

--- backend/src/Room/RoomManager.hpp
+++ backend/src/Room/RoomManager.hpp
@@ -2,6 +2,7 @@
 #include "src/Room/Room.hpp"
 #include <memory>
 #include <vector>
+#include <spdlog/spdlog.h>
 
 namespace FruitsGroove{
 class RoomManager{
@@ -31,6 +32,7 @@ public:
     void CreateRoom(std::array<User, 2>&& userArray){
         auto endedRoomItr = this->FindEndedRoom();
         (*endedRoomItr).reset(new Room{this->latestRoomIndex++, std::move(userArray)});
+        spdlog::info("room created");
     }
 
 };
