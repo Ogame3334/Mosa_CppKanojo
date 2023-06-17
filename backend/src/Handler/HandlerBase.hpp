b26461ea15df03b222f864913eb9207d625c27af
rockcutter <rockcutter414@gmail.com>
2023-06-17 18:31:22 +0900

    [delete] room関連を削除 作り直しのため

--- backend/src/Handler/HandlerBase.hpp
+++ backend/src/Handler/HandlerBase.hpp
@@ -7,7 +7,7 @@
 namespace FruitsGroove{
     class HandlerBase{
     public:
-    virtual void Handle(std::unique_ptr<boost::asio::ip::tcp::socket> socket) = 0;
+    virtual void Handle(std::string_view) = 0;
     };
 }
 
