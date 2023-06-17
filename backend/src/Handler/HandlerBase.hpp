c8562ff4987a394a8e8d05ae979b6f442c5347fb
rockcutter <rockcutter414@gmail.com>
2023-06-17 17:00:19 +0900

    [change] Handleメソッドの引数を変更

--- backend/src/Handler/HandlerBase.hpp
+++ backend/src/Handler/HandlerBase.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <string>
 #include <string_view>
+#include <boost/asio.hpp>
+#include <memory>
 
 namespace FruitsGroove{
     class HandlerBase{
     public:
-        virtual void Handle(std::string_view) = 0;
+    virtual void Handle(std::unique_ptr<boost::asio::ip::tcp::socket> socket) = 0;
     };
 }
 
