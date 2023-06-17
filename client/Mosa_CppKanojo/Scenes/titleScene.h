bcf28113bafb776157cc587eab0354c68d09744e
WantunXD <rechngml@gmail.com>
2023-06-17 19:00:06 +0900

    [add] 全体調整の末なんとか動きます

--- client/Mosa_CppKanojo/Scenes/titleScene.h
+++ client/Mosa_CppKanojo/Scenes/titleScene.h
@@ -1,14 +1,16 @@
 ﻿#pragma once
 # include <Siv3D.hpp>
 
-using App = SceneManager<String>;
-
 // タイトルシーン
 // 挙動 : 左クリックでゲームがスタート
 // 　　　ESCキーで終了
 
 class Title : public App::Scene
 {
+private:
+
+	const Image titlebg_raw{ U"assets/Textures/BackGround/titleBg.png" };
+	const Texture titleBg{ {titlebg_raw.scaled(0.8)}};
 public:
 
 	// コンストラクタ
@@ -17,8 +19,12 @@ public:
 
 	void update() override
 	{
-		// 左クリックで
-		if (MouseL.down())
+		// ESCキーが押されたらゲームを終了
+		if (KeyEscape.pressed()) {
+			System::Exit();
+		}
+		// 左クリックもしくは任意のキー入力で
+		if (MouseL.down() || Keyboard::GetAllInputs())
 		{
 			// 曲選択シーンに遷移
 			changeScene(U"Choice");
@@ -27,11 +33,6 @@ public:
 
 	void draw() const override
 	{
-		Scene::SetBackground(ColorF{ 0.3, 0.4, 0.5 });
-
-		FontAsset(U"TitleFont")(U"My Game").drawAt(750, 100);
-		FontAsset(U"TitleFont")(U">PRESS ANY BUTTON<").drawAt(750, 600);
-
-		Circle{ Cursor::Pos(), 50 }.draw(Palette::Orange);
+		this->titleBg.draw();
 	}
 };
