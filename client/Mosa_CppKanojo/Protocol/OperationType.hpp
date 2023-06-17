ab78499999ed3d667286a75d65e7b4c7a77caf72
rockcutter <rockcutter414@gmail.com>
2023-06-17 22:31:08 +0900

    [add] add OperationType

--- client/Mosa_CppKanojo/Protocol/OperationType.hpp
+++ client/Mosa_CppKanojo/Protocol/OperationType.hpp
@@ -3,7 +3,20 @@
 namespace FruitsGroove{
 enum class OperationType{
     None,
-
-
-};
+    MatchingRequest,
+    WaitForPlayer,
+    MatchingComplete,
+    NoticeIDAndName,
+    NoticeRoomCreated,
+    SelectedSong,
+    WaitForSelectingSong,
+    RandomChoicedSong,  
+    ClientReady, //clientが準備完了であるあかし
+    GameStart,
+    CurrentScore, 
+    CurrentCombo,
+    GameEnd, 
+    ResultScore,
+    OperationCount, // 一番最後に置くこと!!! OperationTypeの数が入る 必要なときもあるだろう
+    };
 }
