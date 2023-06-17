1e8cf9023622d21bbde6bacd230ecc401beed7ef
Ogame3334 <kazuhiroisobe0522@gmail.com>
2023-06-18 06:47:17 +0900

    Room.hppの大量のコメントアウトを削除

--- backend/src/Room/Room.hpp
+++ backend/src/Room/Room.hpp
@@ -6,7 +6,6 @@
 #include <thread>
 #include <string>
 #include "src/User/User.hpp"
-// #include "src/Handler/PacketHandler.hpp"
 
 namespace FruitsGroove{
 class Room{
@@ -21,40 +20,3 @@ public:
     Room(std::array<SocketPtr, 2> sockets);
 };
 }
-// namespace asio = boost::asio;
-// using asio::ip::tcp;
-
-// namespace FruitsGroove{
-// class Room{
-// private:
-//     int roomID;
-//     std::array<User, 2> userArray;
-//     bool isEnded;
-// public:
-//     Room(int roomID, std::array<User, 2>&& userArray):
-//         roomID(roomID),
-//         userArray(std::move(userArray)),
-//         isEnded{false}
-//     {}
-    
-//     bool IsEnded() const{
-//         return this->isEnded;
-//     }
-
-//     void SendMessageToOther(std::string_view userID, std::string_view payload){
-//         tcp::socket* sock;
-//         if(this->userArray.at(0).id == userID){
-//             sock = &(this->userArray.at(1).socket);
-//         }else{
-//             sock = &(this->userArray.at(0).socket);
-//         }
-//         // メッセージ送信
-//         boost::system::error_code error;
-//         asio::write(*sock, asio::buffer(payload), error);
-//         if(error){
-//             spdlog::warn(__func__, ": ", "error");
-//         }
-//         spdlog::info(__func__, ": ", "send message ", payload, " to ", userID);
-//     }
-// };
-// }
