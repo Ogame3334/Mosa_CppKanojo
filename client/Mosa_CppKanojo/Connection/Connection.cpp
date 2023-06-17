002062adba776c67f4bb2728f994bcb62d385476
おがめ <kazuhiroisobe0522@gmail.com>
2023-06-17 16:47:51 +0900

    ServerConnectionクラスを作成し、パケットの送信を可能に

---
+++ client/Mosa_CppKanojo/Connection/Connection.cpp
@@ -0,0 +1,26 @@
+﻿#include "Connection.hpp"
+
+namespace FruitsGroove {
+	namespace Connection {
+		bool ServerConnection::sendPacket(OperationType type, StringView content) {
+			bool couldConnect = false;
+			this->client.connect(this->ip, this->port);
+
+			this->packetBuilder.SetOperation(type).SetContent(content.narrow());
+			std::string packet = this->packetBuilder.Build();
+
+			if (client.isConnected())
+			{
+				client.send(packet.c_str());
+				couldConnect = true;
+			}
+
+			if (client.hasError()) // 切断/接続エラー
+			{
+				client.disconnect();
+			}
+
+			return couldConnect;
+		}
+	}
+}
---
+++ client/Mosa_CppKanojo/Connection/Connection.hpp
@@ -0,0 +1,19 @@
+﻿#include <Siv3D.hpp>
+
+#include "../Protocol/OperationType.hpp"
+#include "../Protocol/PacketBuilder.hpp"
+
+namespace FruitsGroove {
+	namespace Connection {
+		class ServerConnection {
+		private:
+			const IPv4Address ip{ { 219, 94, 241, 220 } };
+			const uint16 port = 23292;
+			TCPClient client;
+			PacketBuilder packetBuilder{};
+		public:
+			ServerConnection() {}
+			bool sendPacket(OperationType type, StringView content);
+		};
+	}
+}
